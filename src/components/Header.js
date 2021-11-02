dc85b39e65b634bbc2ed5bb36939795eac79a8d1
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-02 17:09:35 -0500

    commit tree f9814531252ce089aec809be6c8453296bff4895

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+
+function Header({isDarkMode, onDarkModeClick}) {
+    return (
+        <div>
+            <header>
+                <h2>Shopster</h2>
+                <button onClick={onDarkModeClick}>
+                    {isDarkMode ? "Dark" : "Light"} Mode
+                </button>
+            </header>
+
+        </div>
+    )
+}
+
+export default Header
